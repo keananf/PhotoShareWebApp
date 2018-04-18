ca257d030e694a3fe9d35f6ce8b10acf3517757a
Keanan Frazer <keananf@live.com>
2018-04-18 09:38:29 +0100

    Removed in-memory data-store
    This is not necessary for this project, as only persistent storage via a database will be supported. The in-memory version originated from P2.

--- server/src/main/java/server/datastore/DatabaseResources.java
+++ server/src/main/java/server/datastore/DatabaseResources.java
@@ -3,31 +3,31 @@ package server.datastore;
 /**
  * Class featuring database information (table names, schema, etc).
  */
-public abstract class DatabaseResources {
+abstract class DatabaseResources {
     // Table names
-    public static final String PHOTOS_TABLE = "photos";
-    public static final String COMMENTS_TABLE = "comments";
-    public static final String COMMENTS_VOTES_TABLE = "votes";
-    public static final String USERS_TABLE = "users";
-    public static final String NOTIFICATIONS_TABLE = "notifications";
+    static final String PHOTOS_TABLE = "photos";
+    static final String COMMENTS_TABLE = "comments";
+    static final String COMMENTS_VOTES_TABLE = "votes";
+    static final String USERS_TABLE = "users";
+    static final String NOTIFICATIONS_TABLE = "notifications";
 
     // User table attributes
-    public static final String PARENTNAME = "parentname";
-    public static final String USERNAME = "username";
-    public static final String PASSWORD = "password";
-    public static final String USERS_ADMIN = "admin";
+    static final String PARENTNAME = "parentname";
+    static final String USERNAME = "username";
+    static final String PASSWORD = "password";
+    static final String USERS_ADMIN = "admin";
 
     // Photo table attributes
-    public static final String PHOTOS_ID = "photoId";
-    public static final String PHOTOS_NAME = "photoName";
-    public static final String PHOTOS_CONTENTS = "photoContents";
-    public static final String PHOTOS_TIME = "photoTimestamp";
+    static final String PHOTOS_ID = "photoId";
+    static final String PHOTOS_NAME = "photoName";
+    static final String PHOTOS_CONTENTS = "photoContents";
+    static final String PHOTOS_TIME = "photoTimestamp";
 
     // Comment table attributes
-    public static final String REFERENCE_ID = "referenceId";
-    public static final String COMMENTS_ID = "commentId";
-    public static final String COMMENT_TYPE = "type";
-    public static final String COMMENTS_CONTENTS = "commentContents";
-    public static final String COMMENTS_TIME = "commentTimestamp";
-    public static final String VOTE = "vote";
+    static final String REFERENCE_ID = "referenceId";
+    static final String COMMENTS_ID = "commentId";
+    static final String COMMENT_TYPE = "type";
+    static final String COMMENTS_CONTENTS = "commentContents";
+    static final String COMMENTS_TIME = "commentTimestamp";
+    static final String VOTE = "vote";
 }

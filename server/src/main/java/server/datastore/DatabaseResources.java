34576f02ba78ad958f796ebd76311bcec8a309ce
stuartkumar <stuart.t.kumar@gmail.com>
2018-04-21 10:01:54 +0100

    Bug fix
    Table for Followings is now being cleared properly.

--- server/src/main/java/server/datastore/DatabaseResources.java
+++ server/src/main/java/server/datastore/DatabaseResources.java
@@ -12,7 +12,7 @@ abstract class DatabaseResources {
     static final String COMMENTS_TABLE = "comments";
     static final String COMMENTS_VOTES_TABLE = "votes";
     static final String NOTIFICATIONS_TABLE = "notifications";
-    public static final String FOLLOWINGS_TABLE = "followings";
+    static final String FOLLOWINGS_TABLE = "followings";
 
     // User table attributes
     static final String PARENTNAME = "parentname";
@@ -42,7 +42,7 @@ abstract class DatabaseResources {
 
     // Followings table attributes
 
-    public static final String FOLLOW_ID = "followId";
-    public static final String USER_FROM = "userFrom";
-    public static final String USER_TO = "userTo";
+    static final String FOLLOW_ID = "followId";
+    static final String USER_FROM = "userFrom";
+    static final String USER_TO = "userTo";
 }
